fb3f505438f0725200b2c2e8651f61cbc692af59
snp116 <156687205+binaryBard97@users.noreply.github.com>
2024-09-14 21:19:40 -0400

    Next.js and Database

---
+++ my-app/tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "tailwindcss";
+import tailwindTypography from '@tailwindcss/typography';
+import daisyui from 'daisyui';
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [
+    tailwindTypography,
+    daisyui
+  ],
+};
+export default config;
